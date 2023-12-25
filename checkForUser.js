8072177e53a3a07bf0a0c61fd9335e28ddb88531
Cole Bauml <cole.w.bauml@gmail.com>
2023-12-25 13:32:47 -0800

    checkForUser.js migrated to mongoose. (Not bug tested.)

--- checkForUser.js
+++ checkForUser.js
@@ -1,30 +1,27 @@
-const { adminConnection, connection } = require('./database')
-  
+const {
+    usersModel
+} = require('./database')
 
 //   await checkUserFile.checkForUser(req, res);
 
-const checkForUser = (req, res) => {
-    if (req.session.email === null | req.session.email === undefined) {
-        if (req.session.accountVerified === false) {
-            res.redirect('/verify')
+const checkForUser = async (req, res) => {
+    try {
+        if (req.session.email === null | req.session.email === undefined) {
+            if (req.session.accountVerified === false) {
+                res.redirect('/verify')
+            }
+            else {
+                res.redirect('/login')
+            }
+            return;
         }
-        else {
+        const email = req.session.email;
+        const query = { email: email };
+        var results = await usersModel.find(query)
+        if (results[0].isVerified != "true") {
             res.redirect('/login')
         }
-        return;
-    }
-    const email = req.session.email;
-
-    const query = 'SELECT * FROM users WHERE email = ?';
-    connection.query(query, [email], async (error, results) => {
-        if (error) {
-            console.error(error);
-            return res.status(500).send('An error occurred');
-        }
-        else if (results[0].isVerified != "true") {
-            res.redirect('/login')
-        }
-        else if (results.length === 0) {
+        if (results.length === 0) {
             req.session.destroy((err) => {
                 if (err) {
                     console.error(err);
@@ -32,7 +29,12 @@ const checkForUser = (req, res) => {
                 res.redirect('/login');
             });
         }
-    })
+    } catch (error) {
+        if (error) {
+            console.error(error);
+            return res.status(500).send('An error occurred');
+        }
+    }
 }
 
 module.exports = {
