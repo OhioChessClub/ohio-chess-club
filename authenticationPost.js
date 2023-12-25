b02bcbb59fa4e52c5ab665f39ab90ae5ac76e91f
Cole Bauml <cole.w.bauml@gmail.com>
2023-12-25 13:29:50 -0800

    authenticationPost.js migrated to mongoose. (Not bug tested.)

--- authenticationPost.js
+++ authenticationPost.js
@@ -7,7 +7,9 @@ require('dotenv').config();
 const bcrypt = require('bcrypt')
 
 
-
+const {
+  usersModel
+} = require('./database')
 
 // LOAD EXPRESS AND DEFINE EXPRESS MODULES
 const express = require('express')
@@ -36,9 +38,6 @@ function removeReturn(req, res) {
   req.session.loginReturnURL = null;
 }
 
-// LOAD MYSQL MODULES AND CONNECT TO DB
-const { adminConnection, connection } = require('./database')
-
 function accountNotVerified(req, res, email) {
   req.session.email = email;
   req.session.accountPresent = true;
@@ -55,50 +54,44 @@ function logInAccount(req, res, email) {
 const loginPost = async (req, res) => {
   const { email, password } = req.body;
   if (!isValidEmail(email)) {
-    if(email === process.env.adminEmail){
+    if (email === process.env.adminEmail) {
       // Admin attempting to log in, don't stop it
     }
     else {
       res.render('login', { actionError: "That email is not valid." })
     }
   }
-  const query = `SELECT * FROM users WHERE email = ?`;
-  connection.query(query, [email], async (error, results) => {
-    if (error) {
-      console.error(error);
-      res.render('login', { actionError: `Error logging in: ${error}` })
-    }
-
-    try {
-      if (results.length === 1) {
-        const storedPassword = results[0].password;
-        const passwordMatch = await bcrypt.compare(password, storedPassword);
-        if (passwordMatch) {
-          if (results[0].isVerified === "false" | results[0].isVerified === false) {
-            await accountNotVerified(req, res, email);
-            res.redirect('/verify')
-            return;
+  const query = { email: email };
+  var results = await usersModel.find(query)
+  try {
+    if (results.length === 1) {
+      const storedPassword = results[0].password;
+      const passwordMatch = await bcrypt.compare(password, storedPassword);
+      if (passwordMatch) {
+        if (results[0].isVerified === "false" | results[0].isVerified === false) {
+          await accountNotVerified(req, res, email);
+          res.redirect('/verify')
+          return;
+        }
+        else {
+          await logInAccount(req, res, email)
+          if (req.session.loginReturnURL != null && req.session.loginReturnURL != undefined) {
+            const returnUrl = req.session.loginReturnURL;
+            await removeReturn(req, res)
+            return res.redirect(returnUrl)
           }
-          else {
-            await logInAccount(req, res, email)
-            if (req.session.loginReturnURL != null && req.session.loginReturnURL != undefined) {
-              const returnUrl = req.session.loginReturnURL;
-              await removeReturn(req, res)
-              return res.redirect(returnUrl)
-            }
-            else { return res.redirect('/') };
-          }
-        } else {
-          res.render('login', { actionError: "Incorrect password for that email." })
+          else { return res.redirect('/') };
         }
       } else {
-        res.render('login', { actionError: "No user with that email exists." })
+        res.render('login', { actionError: "Incorrect password for that email." })
       }
-    } catch (error) {
-      console.error(error);
-      res.render('login', { actionError: `Error logging in: ${error}` })
+    } else {
+      res.render('login', { actionError: "No user with that email exists." })
     }
-  });
+  } catch (error) {
+    console.error(error);
+    res.render('login', { actionError: `Error logging in: ${error}` })
+  }
 };
 
 const generateRandomSixDigitNumber = () => {
@@ -114,13 +107,13 @@ const registerPost = async (req, res) => {
     var noPassError = "Please enter a password."
     var noEmailError = "Please enter a valid email."
     var noNameError = "Please enter a name."
-    if(password === null || password === undefined || password === ""){
+    if (password === null || password === undefined || password === "") {
       res.render('register', { actionError: noPassError })
     }
-    if(name === null || name === undefined || name === ""){
+    if (name === null || name === undefined || name === "") {
       res.render('register', { actionError: noNameError })
     }
-    if(email === null || email === undefined || email === "" || !isValidEmail(email)){
+    if (email === null || email === undefined || email === "" || !isValidEmail(email)) {
       res.render('register', { actionError: noEmailError })
     }
     else if (country && city === null || country && city === undefined || country && city === "") {
@@ -133,27 +126,22 @@ const registerPost = async (req, res) => {
       res.render('register', { actionError: nullError })
     }
     else {
-
-      const query = `SELECT * FROM users WHERE email = ?`;
-      connection.query(query, [email], async (error, results) => {
-        if (error) {
-          console.error(error);
-          res.redirect(process.env.registerPage, { actionError: error })
-        } else {
-          if (results.length > 0) {
-            res.render('register', { actionError: "A user with that email already exists." })
+      const query = { email: email };
+      var results = await usersModel.find(query)
+      if (results.length > 0) {
+        res.render('register', { actionError: "A user with that email already exists." })
+      } else {
+        const hashedPassword = await bcrypt.hash(req.body.password, 10);
+        const verificationCode = generateRandomSixDigitNumber();
+        const insertQuery = 'INSERT INTO users (name, email, password, verificationCode, isVerified, country, city, state) VALUES (?, ?, ?, ?, ?, ?, ?, ?)';
+        connection.query(insertQuery, [name, email, hashedPassword, verificationCode, "false", country, city, state], async (error, results) => {
+          if (error) {
+            console.error(error);
+            res.render('register', { actionError: error })
           } else {
-            const hashedPassword = await bcrypt.hash(req.body.password, 10);
-            const verificationCode = generateRandomSixDigitNumber();
-            const insertQuery = 'INSERT INTO users (name, email, password, verificationCode, isVerified, country, city, state) VALUES (?, ?, ?, ?, ?, ?, ?, ?)';
-            connection.query(insertQuery, [name, email, hashedPassword, verificationCode, "false", country, city, state], async (error, results) => {
-              if (error) {
-                console.error(error);
-                res.render('register', { actionError: error })
-              } else {
-                // req.session.loggedIn = true;
-                accountNotVerified(req, session, email)
-                const contents = `
+            // req.session.loggedIn = true;
+            accountNotVerified(req, session, email)
+            const contents = `
 <div class="container">
   <div class="content">
     <h1>Thanks for signing up!</h1>
@@ -169,20 +157,18 @@ const registerPost = async (req, res) => {
 </div>
   `
 
-                const info = await transporter.sendMail({
-                  from: `Ohio Chess Club <ohiochessclub@gmail.com>`,
-                  to: email,
-                  subject: "Verification Code",
-                  html: contents,
-                });
-
-                res.redirect('/verify');
-
-              }
+            await transporter.sendMail({
+              from: `Ohio Chess Club <ohiochessclub@gmail.com>`,
+              to: email,
+              subject: "Verification Code",
+              html: contents,
             });
+
+            res.redirect('/verify');
+
           }
-        }
-      });
+        });
+      }
     }
   } catch (error) {
     console.error(error);
@@ -190,85 +176,83 @@ const registerPost = async (req, res) => {
   }
 };
 const verifyPost = async (req, res) => {
-
-  const { enteredCode } = req.body;
-  if (enteredCode === null || enteredCode.length > 6 || enteredCode < 0) {
-    var actionError = "Number cannot be less than zero or more than six characters."
-    res.render('verify', { actionError: actionError, email: req.session.email })
-    return
-  }
-  else if (req.session.email === null | req.session.email === undefined) {
-    res.redirect('login')
-    return
-  }
-  const sqlcorrectverify = "UPDATE `users` SET `isVerified` = 'true' WHERE `users`.`email` = ?;";
-  const checkcodesql = "SELECT * FROM users WHERE email = ?;";
-
-  connection.query(checkcodesql, [req.session.email], async (error, results) => {
-    if (error) {
-      console.log(error);
-      res.redirect('verify');
-      return;
+  try {
+    const { enteredCode } = req.body;
+    if (enteredCode === null || enteredCode.length > 6 || enteredCode < 0) {
+      var actionError = "Number cannot be less than zero or more than six characters."
+      res.render('verify', { actionError: actionError, email: req.session.email })
+      return
+    }
+    else if (req.session.email === null | req.session.email === undefined) {
+      res.redirect('login')
+      return
     }
 
+    const sqlcorrectverify = "UPDATE `users` SET `isVerified` = 'true' WHERE `users`.`email` = ?;";
+    const checkcodesql = "SELECT * FROM users WHERE email = ?;";
+    const query = {
+      email: req.session.email
+    }
+
+    const filter = { email: req.session.email }
+    const results = await usersModel.find(filter)
     const code = results[0].verificationCode.toString();
 
     if (enteredCode === code) {
-      adminConnection.query(sqlcorrectverify, [req.session.email], async (error, results) => {
-        if (error) {
-          console.log(error);
-          res.redirect('verify');
-          return;
-        }
-        const contents = `
-          <div class="container">
-            <div class="content">
-              <h1>Thanks for signing up!</h1>
-              <p>Dear User,</p>
-              <p>Thank you for verifying your email for the Ohio Chess Club.</p>
-              <p>If you have any questions or need assistance, feel free to contact our support team.</p>
-            </div>
-            <div class="footer">
-              <p>This email was sent to you by the <b>Ohio Chess Club</b>.</p>
-              <p>Please do not reply to this email.</p>
-            </div>
+      usersModel.findOneAndUpdate(query, { isVerified: 'true' })
+      const contents = `
+        <div class="container">
+          <div class="content">
+            <h1>Your email is now verified!</h1>
+            <p>Dear User,</p>
+            <p>Thank you for verifying your email for the Ohio Chess Club.</p>
+            <p>If you have any questions or need assistance, feel free to contact our support team.</p>
           </div>
-            `
-        const info = await transporter.sendMail({
-          from: `Ohio Chess Club <ohiochessclub@gmail.com>`,
-          to: req.session.email,
-          subject: "Email Verified",
-          html: contents,
-        });
-        var email = req.session.email;
-        logInAccount(req, res, email)
-        res.redirect('/login');
+          <div class="footer">
+            <p>This email was sent to you by the <b>Ohio Chess Club</b>.</p>
+            <p>Please do not reply to this email.</p>
+          </div>
+        </div>
+          `
+      const info = await transporter.sendMail({
+        from: `Ohio Chess Club <ohiochessclub@gmail.com>`,
+        to: req.session.email,
+        subject: "Email Verified",
+        html: contents,
       });
+      var email = req.session.email;
+      logInAccount(req, res, email)
+      res.redirect('/login');
     } else {
       res.render('verify', { actionError: "Incorrect code. Try again.", email: req.session.email });
     }
-  });
+  } catch (error) {
+    res.render('verify', { actionError: "There was an error verifying your account. Please send a contact request to let us know, and we will help you out. Error: " + error })
+    console.log("ERROR VERIFYING ACCOUNT: " + error)
+  }
 };
 
 
-const forgotPasswordPost = async(req, res) => {
-  const { email } = req.body;
-  var emailIsValid = isValidEmail(email);
-  if(emailIsValid === true || email === process.env.adminEmail){
-    var query = "SELECT * FROM `users` WHERE email = ?";
-    adminConnection.query(query, [email], async(error, results) => {
-      if(error){
-        res.render('forgotPassword', { actionError: "There was an unknown error while reseting your passcode. Our developers have been notified and are looking in to it. If this issue persists, feel free to use the contact form so we can help you directly. Thanks so much!"})
-      }
-      else{
-        console.log(results)
-      }
-    })
+const forgotPasswordPost = async (req, res) => {
+  try {
+    const { email } = req.body;
+    var emailIsValid = isValidEmail(email);
+    if (emailIsValid === true || email === process.env.adminEmail) {
+      var query = "SELECT * FROM `users` WHERE email = ?";
+      const filter = { email: email }
+      var results = await usersModel.find(filter)
+    }
+    else {
+      res.render('forgotPassword', { actionError: "That email is not a valid email." })
+    }
+  } catch (error) {
+    if (error) {
+      res.render('forgotPassword', { actionError: "There was an unknown error while reseting your passcode. Our developers have been notified and are looking in to it. If this issue persists, feel free to use the contact form so we can help you directly. Thanks so much!" })
+    }
+    else {
+      console.log(results)
+    }
   }
-  else {
-    res.render('forgotPassword',  {actionError: "That email is not a valid email."} )
-  }
-
 }
 
 
